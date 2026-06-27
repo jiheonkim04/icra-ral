cd532f78cdf46c19fb47c215369ad96daa90b4e1
jiheonkim04 <overwatch31697@gmail.com>
2026-06-27 18:31:13 +0900

    Export distributional TCA-Select helpers

--- tca_map/inference/__init__.py
+++ tca_map/inference/__init__.py
@@ -1,15 +1,27 @@
 from .tca_select import (
+    distributional_tca_select_inference,
+    heatmap_entropy,
+    heatmap_js,
+    heatmap_kl,
+    normalize_heatmap_distribution,
     sample_heatmap_candidates,
-    score_candidate_target_consistency,
     score_candidate_condition_sensitivity,
+    score_candidate_distributional,
+    score_candidate_target_consistency,
     select_tca_candidate,
     tca_select_inference,
 )
 
 __all__ = [
+    "distributional_tca_select_inference",
+    "heatmap_entropy",
+    "heatmap_js",
+    "heatmap_kl",
+    "normalize_heatmap_distribution",
     "sample_heatmap_candidates",
-    "score_candidate_target_consistency",
     "score_candidate_condition_sensitivity",
+    "score_candidate_distributional",
+    "score_candidate_target_consistency",
     "select_tca_candidate",
     "tca_select_inference",
 ]
